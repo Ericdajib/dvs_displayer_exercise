9044496071546e74504f94b950894fbffdeb924a
Ericdajib <xc1044220200@gmail.com>
2022-05-25 00:57:01 +0200

    Initial commit

---
+++ include/dvs_displayer/displayer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <dvs_msgs/EventArray.h>
+#include <opencv2/core/core.hpp>
+
+
+namespace dvs_displayer
+{
+
+    class Displayer {
+    public:
+        Displayer(ros::NodeHandle & nh, ros::NodeHandle nh_private);
+        virtual ~Displayer();
+
+    private:
+        ros::NodeHandle nh_;
+
+        // Callback functions
+        void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg);
+        void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
+
+
+        // Subscribers
+        ros::Subscriber event_sub_;
+        image_transport::Subscriber image_sub_;
+
+        // Publishers
+        image_transport::Publisher image_pub_;
+
+
+        enum DisplayMethod
+        {
+            GRAYSCALE, RED_BLUE
+        } display_method_;
+
+        cv::Mat last_image_;
+        bool used_last_image_;
+
+    };
+
+} // namespace
